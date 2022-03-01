ab6c5245ad720f64c58d982215e12d10666dc29a
shashwatbangar <shashwatbangar@gmail.com>
2022-03-01 14:16:51 +0530

    Two Sum explained

--- Easy/1. Two Sum/solution.js
+++ Easy/1. Two Sum/solution.js
@@ -1,3 +1,18 @@
+// Time Complexity = O(n^2)
+// Space Complexity = O(1)
+
+var twoSum = function(nums, target) {
+    for(var i=0; i<nums.length; i++) {
+        for(var j=i+1; j<nums.length; j++) {
+            if(nums[i]+nums[j] === target)  return [i, j];
+        }
+    }
+};
+
+
+// Time Complexity = O(n)
+// Space Complexity = O(n)
+
 var twoSum = function(nums, target) {
     let map = {}
     
