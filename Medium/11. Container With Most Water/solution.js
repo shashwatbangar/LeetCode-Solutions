3fe6aa7f56253f741cf08d28dde01df439a620dd
shashwatbangar <shashwatbangar@gmail.com>
2022-03-07 17:42:59 +0530

    added solution 11

---
+++ Medium/11. Container With Most Water/solution.js
@@ -0,0 +1,19 @@
+// Problem Statement: https://leetcode.com/problems/container-with-most-water/
+
+// Time Complexity - O(n)
+// Space Complexity - O(1)
+
+var maxArea = function(height) {
+    let result = 0, left = 0, right = height.length - 1;
+
+	while (left < right) {
+		let smallestSide = Math.min(height[left], height[right]);
+		let area = (right - left) * smallestSide;
+
+		if (area > result) result = area;
+
+		if (height[left] < height[right]) left++;
+		else right--;
+	}
+    return result;
+};
